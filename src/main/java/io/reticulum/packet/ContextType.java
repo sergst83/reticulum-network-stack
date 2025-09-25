d41675bed978c8c728ef6561c5e5fdcac55a935b
Belousov Oleg <belousov.oleg@gmail.com>
2025-09-25 15:31:05 +0300

    Implemented ratchets

--- src/main/java/io/reticulum/packet/ContextType.java
+++ src/main/java/io/reticulum/packet/ContextType.java
@@ -1,4 +1,4 @@
-package io.reticulum.transport;
+package io.reticulum.packet;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,14 +7,13 @@ import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
-public enum TransportType {
-
-    BROADCAST((byte) 0x00),
-    TRANSPORT((byte) 0x01);
+public enum ContextType {
+    FLAG_UNSET((byte) 0x00),
+    FLAG_SET((byte) 0x01);
 
     private final byte value;
 
-    public static TransportType fromValue(final byte value) {
+    public static ContextType fromValue(final byte value) {
         return Arrays.stream(values())
                 .filter(type -> type.getValue() == value)
                 .findFirst()
